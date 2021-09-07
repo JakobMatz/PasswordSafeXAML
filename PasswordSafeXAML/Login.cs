b2d74b1f6061495a83bf110c0ca52bee36daf812
Philipp-Voss <83300730+Philipp-Voss@users.noreply.github.com>
2021-09-07 10:17:29 +0200

    Login Class hinzugefügt

---
+++ PasswordSafeXAML/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordSafeXAML
+{
+    class Login
+    {
+        private string _pw = "";
+        public Login(string pw)
+        {
+            _pw = pw;
+        }
+        private bool pwCheck()
+        {
+            string password = "asdf";
+
+            if(_pw.Equals(password))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
