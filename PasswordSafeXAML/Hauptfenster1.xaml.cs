f998ad1b1d1779bc64887e1108e2c19fe05f4a6e
JakobMatz <37841778+JakobMatz@users.noreply.github.com>
2021-09-08 10:19:11 +0200

    Window1TestDone

--- PasswordSafeXAML/Hauptfenster1.xaml.cs
+++ PasswordSafeXAML/Hauptfenster1.xaml.cs
@@ -23,5 +23,10 @@ namespace PasswordSafeXAML
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
