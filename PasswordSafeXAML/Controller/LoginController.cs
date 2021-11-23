ff34fbf69665f2bbcf605c732913a3dfe2f2a9fe
JakobMatz <37841778+JakobMatz@users.noreply.github.com>
2021-11-23 12:11:44 +0100

    Addes some cool stuff like controller, views and LoginViews

---
+++ PasswordSafeXAML/Controller/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using PasswordSafeXAML.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordSafeXAML.Controller
+{
+    class LoginController
+    {
+        private static List<LoginModel> LoginData = new List<LoginModel>();
+        public static void newLogin(string eMail, string loginname, string password, string website, string description)
+        {
+            MessageBox.Show(eMail);
+            LoginData.AddRange(new List<LoginModel> {
+            new LoginModel(eMail,loginname,password,website,description)
+            });
+
+            try
+            {
+                FileStream addLoginData = new FileStream("AddedLogins.txt", FileMode.OpenOrCreate);
+                using (StreamWriter ASKYWRITE = new StreamWriter(addLoginData))
+                {
+                    ASKYWRITE.Write(eMail + "\r\n");
+                    ASKYWRITE.Write(loginname + "\r\n");
+                    ASKYWRITE.Write(password + "\r\n");
+                    ASKYWRITE.Write(website + "\r\n");
+                    ASKYWRITE.Write(description + "\r\n");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error");
+            }
+        }
+    }
+}
---
+++ PasswordSafeXAML/Views/AddLoginView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordSafeXAML.Views
+{
+    /// <summary>
+    /// Interaktionslogik für AddLoginView.xaml
+    /// </summary>
+    public partial class AddLoginView : Window
+
+    {
+        private string eMailInput;
+        private string loginnameInput;
+        private string passwortInput;
+        private string websiteInput;
+        private string descriptionInput;
+        public AddLoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.eMailInput = eMail.Text;
+            this.loginnameInput = loginname.Text;
+            this.passwortInput = password.Text;
+            this.websiteInput = website.Text;
+            this.descriptionInput = description.Text;
+
+            Controller.LoginController.newLogin(this.eMailInput,this.loginnameInput,this.passwortInput, 
+                this.websiteInput, this.descriptionInput);
+        }
+    }
+}
