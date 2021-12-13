d21b32c0c27bc43f958ae6f8f91f3005a0bba8fc
JakobMatz <37841778+JakobMatz@users.noreply.github.com>
2021-12-13 18:22:06 +0100

    Added csv reading

--- PasswordSafeXAML/Hauptfenster1.xaml.cs
+++ PasswordSafeXAML/Hauptfenster1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PasswordSafeXAML.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,41 @@ namespace PasswordSafeXAML
     /// <summary>
     /// Interaktionslogik für Window1.xaml
     /// </summary>
+    /// 
+
+
+    public class TableNames
+
+    {
+
+        public string t1;
+
+        public string t2;
+
+        public string t3;
+
+        public static TableNames FromCsv(string csvLine)
+
+        {
+
+            string[] values = csvLine.Split(';');
+
+            TableNames dailyValues = new TableNames();
+
+            dailyValues.t1 = values[0];
+
+            dailyValues.t2 = values[1];
+
+            dailyValues.t3 = values[2];
+
+            return dailyValues;
+
+        }
+
+    }
+
+
+
     public partial class Window1 : Window
     {
         public Window1()
@@ -30,10 +66,31 @@ namespace PasswordSafeXAML
 
         }
 
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             AddLoginView win = new AddLoginView();
             win.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<TableNames> values = File.ReadAllLines("C:\\Users\\jakob\\Documents\\git\\PasswordSafeXAML\\PasswordSafeXAML\\bin\\Debug\\LoginDaten.csv")
+
+                              .Skip(1)
+
+                              .Select(v => TableNames.FromCsv(v))
+
+                              .ToList();
+
+            foreach (var item in values)
+
+            {
+
+                ListBox1.Items.Add(item.t1);
+
+            }
         }
     }
 }
