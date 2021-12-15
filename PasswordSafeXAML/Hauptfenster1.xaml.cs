545d2cd7d77fe8a584b84f14fa4f6c9de0e2d716
JakobMatz <37841778+JakobMatz@users.noreply.github.com>
2021-12-15 13:16:52 +0100

    Added CSV reading

--- PasswordSafeXAML/Hauptfenster1.xaml.cs
+++ PasswordSafeXAML/Hauptfenster1.xaml.cs
@@ -1,7 +1,9 @@
-﻿using PasswordSafeXAML.Views;
+﻿using Microsoft.Win32;
+using PasswordSafeXAML.Views;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace PasswordSafeXAML
 {
@@ -39,11 +42,72 @@ namespace PasswordSafeXAML
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           
+
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            {
+
+                List<example> values = File.ReadAllLines("LoginDaten.csv")
+
+                                              .Skip(1)
+
+                                              .Select(v => example.FromCsv(v))
+
+                                              .ToList();
+
+                foreach (var item in values)
+
+                {
+
+                    listView1.Items.Add(new { accountname = item.t1, email =  item.t2, loginname = item.t3, password = item.t4, website = item.t5, description = item.t6});
+
+                }
+
+            }
+        }
+
+
+        public class example
+
+        {
+
+            public string t1;
+
+            public string t2;
+
+            public string t3;
+
+            public string t4;
+
+            public string t5;
+
+            public string t6;
+
+            public static example FromCsv(string csvLine)
+
+            {
+
+                string[] values = csvLine.Split(';');
+
+                example dailyValues = new example();
+
+                dailyValues.t1 = values[0];
+
+                dailyValues.t2 = values[1];
+
+                dailyValues.t3 = values[2];
+
+                dailyValues.t4 = values[3];
+
+                dailyValues.t5 = values[4];
+
+                dailyValues.t6 = values[5];
+
+                return dailyValues;
+
+            }
 
         }
     }
